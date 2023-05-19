c8a5b4c941c9e99d2f91add0a158102e68a57ee8
CaoDuc <caonhatduc9@gmail.com>
2023-05-19 11:03:03 +0700

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const corsOptions: CorsOptions = {
+    origin: 'http://localhost:3000', // add rontend url here
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true,
+  };
+  app.enableCors(corsOptions)
   await app.listen(3500);
 }
 bootstrap();
